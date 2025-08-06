47896116e31bd1c45914a9ffe7e7195ec7a5461c
Amna BB <theamnabb@gmail.com>
2025-08-06 12:29:58 +0500

    Add sum function using lambda and improve formatting

--- Lambda.py
+++ Lambda.py
@@ -10,3 +10,15 @@ def myfunc(n):
 mydoubler = myfunc(2)
 
 print(mydoubler(11))
+
+
+# Sum 0f two number
+
+sumTwo = lambda a,b: a+b
+print(f'The sum of two number is: ',sumTwo(2,4))
+
+
+
+# def function 
+
+
