028e00019bd1b10d7432f311b8bbd75d89611cd6
Amna BB <theamnabb@gmail.com>
2025-08-06 12:57:10 +0500

    added Append

--- FileHandling/firstfile.py
+++ FileHandling/firstfile.py
@@ -1,6 +1,10 @@
 f = open("myfile.txt", "w")  # creates file if not exists, or overwrites if exists
-f.write("Hello world!")
+f.write("Hello world!\n")
 
 
 with open('myfile.txt', 'a') as f:
     f.write("Appending this line.\n")
+
+
+with open("demofile.txt") as f:
+  print(f.read())
