4cbca48aed9c5250c328fbe9def11282bbf37fae
Amna BB <theamnabb@gmail.com>
2025-09-17 22:54:13 +0500

    Class 2

---
+++ classs2.py
@@ -0,0 +1,17 @@
+# Data Type 17 Sep 2025
+
+x = 2  # intger number 
+print(x)
+print(type(x))
+
+
+
+name = "john"  # string
+print(name)
+print(type(name))
+
+floating = 2.5  # floating point number
+print(floating)
+print(type(floating))
+
+
