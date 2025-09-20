ed9139342569bfc8a9cfcad556655f031df696ba
Amna BB <theamnabb@gmail.com>
2025-09-20 21:44:36 +0500

    Operators Done

--- classs2.py
+++ classs2.py
@@ -1,25 +1,48 @@
  # converting string to integer
 
 
-a =  5
-b =  3
+# a =  5
+# b =  3
 
-print(a + b)
-print(a - b)
-print(a * b)
-print(a / b)
-print(a // b)
-print(a % b)
-print(a ** b)
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# print(a ** b)
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
 
-sum = num1 + num2
-print("The sum is:", sum)
+# sum = num1 + num2
+# print("The sum is:", sum)
 
-subtr = num1 -num2
-print("The subtraction is:", subtr)
+# subtr = num1 -num2
+# print("The subtraction is:", subtr)
 
 # Assignment Operators 
+
+#  
+
+x = 5 
+print(x)
+
+x -= 3  # x = x + 3
+print(x)
+
+x = 5 
+y = 6
+
+print(x<=y)
+print(x==y)
+print(x!=y)
+
+
+a = True
+
+b = False
+
+print(a and b) 
+
 
