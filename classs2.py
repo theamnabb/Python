f2dc43532d5c7f8f6cfd615199b1dd23d74531c4
Amna BB <theamnabb@gmail.com>
2025-09-25 22:22:44 +0500

    loops

--- classs2.py
+++ classs2.py
@@ -1,51 +1,71 @@
- # converting string to integer
+#  # converting string to integer
 
 
-# a =  5
-# b =  3
+# # a =  5
+# # b =  3
 
-# print(a + b)
-# print(a - b)
-# print(a * b)
-# print(a / b)
-# print(a // b)
-# print(a % b)
-# print(a ** b)
+# # print(a + b)
+# # print(a - b)
+# # print(a * b)
+# # print(a / b)
+# # print(a // b)
+# # print(a % b)
+# # print(a ** b)
 
-# num1 = int(input("Enter first number: "))
-# num2 = int(input("Enter second number: "))
+# # num1 = int(input("Enter first number: "))
+# # num2 = int(input("Enter second number: "))
 
-# sum = num1 + num2
-# print("The sum is:", sum)
+# # sum = num1 + num2
+# # print("The sum is:", sum)
 
-# subtr = num1 -num2
-# print("The subtraction is:", subtr)
+# # subtr = num1 -num2
+# # print("The subtraction is:", subtr)
 
-# Assignment Operators 
+# # Assignment Operators 
 
-#  
+# #  
 
-x = 5 
-print(x)
+# x = 5 
+# print(x)
 
-x -= 3  # x = x + 3
-print(x)
+# x -= 3  # x = x + 3
+# print(x)
 
-x = 5 
-y = 6
+# x = 5 
+# y = 6
 
-print(x<=y)
-print(x==y)
-print(x!=y)
-
-
-a = True
-
-b = False
-
-print(a and b) 
-
-# 22-sep - 2025 
-# FLow control statements
+# print(x<=y)
+# print(x==y)
+# print(x!=y)
 
 
+# a = True
+
+# b = False
+
+# print(a and b) 
+
+# # 22-sep - 2025 
+# # FLow control statements
+
+
+
+
+# # Loop 
+
+# i = 1
+# while i <=5:
+#     print(i)
+#     print("Hello")
+#     i += 1
+
+# print("Out of loop")
+
+
+
+dayofwek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+for day in dayofwek:
+    if day == 'Fri':
+        break
+    print(day)
